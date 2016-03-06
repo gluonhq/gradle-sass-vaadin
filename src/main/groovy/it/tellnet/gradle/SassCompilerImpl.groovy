1abb51c9789be6c062360360fb011ae1abb58967
eryzhikov <eugene.ryzhikov@gluonhq.com>
2016-03-06 17:48:51 -0500

    Implement a better "invisible" way of handling "null clash". Original scss files are copied and used for compilation with nulls replaced by fake values in advance. After compilation, fake values are replaced by nulls in resulting css files.

--- src/main/groovy/it/tellnet/gradle/SassCompilerImpl.groovy
+++ src/main/groovy/it/tellnet/gradle/SassCompilerImpl.groovy
@@ -16,29 +16,44 @@ class SassCompilerImpl {
 
 
     def exec(){
-        SCSSErrorHandler handler
-        if(silent)
-            handler = new SilentErrorHandler()
-        else
-            handler = new SCSSErrorHandler()
-        ScssStylesheet sass = ScssStylesheet.get(scss.absolutePath, null, new SCSSDocumentHandlerImpl(),handler)
-        sass.setFile(scss)
+        SCSSErrorHandler handler = silent? new SilentErrorHandler(): new SCSSErrorHandler()
+
+        final def fakeValue = "gluon-null"
+
+        def sourceScss = scss
+
+        if ( javafx ) {
+            // create tempFile to preserve the original scss
+            sourceScss = new File('tmp_' + scss.getName())
+            sourceScss << scss.text
+
+            // Replace all null values with fake ones
+            sourceScss.write( sourceScss.text.replaceAll(':\\s*null\\s*;', ": $fakeValue;") )
+        }
+
+        ScssStylesheet sass = ScssStylesheet.get(sourceScss.absolutePath, null, new SCSSDocumentHandlerImpl(),handler)
+        sass.setFile(sourceScss)
 
         // setting charset breaks JavaFX CSS generation
         // and should be turned off for JavaFX applications
         if ( !javafx ) {
             sass.setCharset('UTF-8')
+
         }
         sass.addResolver(resolver.getFSResolver())
         sass.addResolver(resolver)
         def basename = scss.getName().replaceAll('\\.scss','.css')
         sass.compile()
+
         File file = new File(outDir.getAbsolutePath() + File.separator + basename)
         file.withWriter {
             sass.write(it, minify)
         }
+
         if (javafx) {
-            file.write( file.text.replaceAll(':\\s*nil\\s*;', ': null;') )
+            // replace all fakeValues with nulls as it supposed to be
+            file.write( file.text.replaceAll(":\\s*$fakeValue\\s*;", ': null;') )
+            sourceScss.delete() // delete temp file created only in javafx mode
         }
     }
 
